e12f024652c9f2cbf4ce85cfc5b630a919f80670
Will Drougas <wdrougas@gmail.com>
2020-03-15 09:56:30 -0400

    solution2

---
+++ solution2.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb://localhost/mongo-exercises')
+
+
+const courseScheman = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: Date,
+    isPublished: Boolean,
+    price: Number
+})
+
+const Course = mongoose.model("Course", courseScheman)
+
+
+async function getCourses() {
+    return await Course
+    .find({isPublished: true, tags: { $in: ['frontend', 'backend']}})
+    .sort({price: -1})
+    .select({name: 1, author: 1, price: 1})
+}
+
+async function run() {
+    const courses = await getCourses()
+    console.log(courses)
+}
+
+run()
